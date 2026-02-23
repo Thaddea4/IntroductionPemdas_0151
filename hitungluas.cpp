31735a48e97323a906c83b67a3a78ad4d595096b
Thaddea4 <adeyasyie@gmail.com>
2026-02-23 16:04:53 +0700

    Menambahkan wadah library

--- hitungluas.cpp
+++ hitungluas.cpp
@@ -1,16 +1,2 @@
 #include <iostream>
 using namespace std;
-
-int main(){
-    int p, l, luas;
-
-    cout << "Masukan panjang: ";
-    cin >> p;
-
-    cout << "Masukan lebar: ";
-    cin >> l;
-    
-    luas = p*l;
-
-    cout << "Hasilnya" << luas;
-}
